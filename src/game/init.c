216414c16db3d2f5fc61244f51fa172d4ba10cbc
Mathieu Roy <maroy@c1r8p5.42quebec.com>
2024-01-15 19:19:05 -0500

    Map dans la fenetre youpi
    TODO: refactor

--- src/game/init.c
+++ src/game/init.c
@@ -6,7 +6,7 @@
 /*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 01:41:59 by maroy             #+#    #+#             */
-/*   Updated: 2024/01/15 15:59:11 by maroy            ###   ########.fr       */
+/*   Updated: 2024/01/15 19:18:04 by maroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,13 +85,85 @@ char	**make_char_map(t_list *raw_map)
 	return (map);
 }
 
+
+#define SIZE 64
+
 char	*init_graphics(t_cub_file *cub, t_game *game)
 {
 	(void)cub;
-	game->mlx = mlx_init( WIN_X, WIN_Y, WIN_TITLE, FALSE);
+
+	
+	mlx_set_setting(MLX_MAXIMIZED, true);
+	game->mlx = mlx_init( WIN_X, WIN_Y, WIN_TITLE, true);
 	if (!game->mlx)
 		return (GRAPHICS_INIT);
+		
+
+	int i;
+	int j;
+	
+	j = 0;
+	i = 0;
+    mlx_image_t* img_un = mlx_new_image(game->mlx, SIZE, SIZE);
+
+	mlx_image_t* img_zero = mlx_new_image(game->mlx, SIZE, SIZE);
+
+	while (i < game->map_h)
+	{
+		j = 0;
+		while (j < game->map_w)
+		{
+			if (game->map[i][j] == '1')
+			{
+				mlx_image_to_window(game->mlx, img_un, j * SIZE, i * SIZE);
+				u_int32_t x = 0;
+				u_int32_t y = 0;
+				while(x < img_un->width - 1)
+				{
+					y = 0;
+					while(y < img_un->height - 1)
+					{
+						mlx_put_pixel(img_un, x, y, 0x484e52FF);
+						y++;
+					}
+					x++;
+				}
+			}
+			else if (game->map[i][j] == '0')
+			{
+				mlx_image_to_window(game->mlx, img_zero, j * SIZE, i * SIZE);
+				u_int32_t x = 0;
+				u_int32_t y = 0;
+				while(x < img_zero->width - 1)
+				{
+					y = 0;
+					while(y < img_zero->height - 1 )
+					{
+						mlx_put_pixel(img_zero, x, y, 0xafb9c5FF);
+						y++;
+					}
+					x++;
+				}
+			}
+			// else if (game->map[i][j] == 'E' || game->map[i][j] == 'W'
+			// 	|| game->map[i][j] == 'S' || game->map[i][j] == 'N')
+			// {
+			// 	mlx_image_to_window(game->mlx, img, j * 8, i * 8);
+			// }
+			// else
+			// {
+			// 	mlx_image_to_window(game->mlx, img, j * 8, i * 8);
+			// }
+			j++;
+		}
+		i++;
+	}
+
+
+	
+
 	mlx_loop(game->mlx);
+	mlx_terminate(game->mlx);
 	return (NULL);
 }
 
@@ -101,10 +173,8 @@ char	*init_game(t_cub_file *cub, t_game *game)
 	game->map = make_char_map(cub->raw_map);
 	if (!game->map)
 		return (MALLOC_CHARMAP);
-
 	game->map_w = max_list_str_len(cub->raw_map);
 	game->map_h = ft_lstsize(cub->raw_map);
-
 	debug_print_map(game);
 	return (init_graphics(cub, game));
 }
